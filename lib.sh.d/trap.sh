586e6c487aabaa1f44a023983baa618e0e2169ec
Ivan Shapovalov <intelfx@intelfx.name>
2020-11-01 08:54:42 +0300

    lib.sh: trap: add usage example

--- lib.sh.d/trap.sh
+++ lib.sh.d/trap.sh
@@ -4,6 +4,22 @@
 # Every time I get back to writing in bash, I end up writing a (new)
 # library that does awful things with traps.
 #
+# Usage:
+#  function foo() {
+#  	eval "$(ltraps)"
+#
+#  	ltrap "echo foo"
+#  	ltrap "echo bar"
+#  	ltrap "rm -rf /"
+#  	luntrap
+#  }
+#
+#  eval "$(globaltraps)"
+#  ltrap "echo foo"
+#  ltrap "echo bar"
+#  ltrap "rm -rf /"
+#  luntrap
+#
 
 ltraps() {
 	cat <<-"EOF"
