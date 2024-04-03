86ae25733632adc84af47da85f40863266a73a69
Ivan Shapovalov <intelfx@intelfx.name>
2024-04-04 00:37:07 +0200

    lib.sh: mktemp: add wrappers around mktemp with cleanup via globaltraps

---
+++ lib.sh.d/mktemp.sh
@@ -0,0 +1,34 @@
+#!/hint/bash
+
+libmktemp() {
+	if ! [[ $_HAVE_CLEANUP_FILES ]]; then
+		die "libmktemp() called before libmktemp_setup()"
+	fi
+
+	_CLEANUP_FILES+="$(command mktemp --tmpdir "${0##*/}${1:+"-$1"}.XXXXXXXXXX" "${@:2}")"
+	echo "${_CLEANUP_FILES[-1]}"
+}
+
+libmktemp_cleanup() {
+	if ! [[ $_HAVE_CLEANUP_FILES ]]; then
+		die "libmktemp_cleanup() called before libmktemp_setup()"
+	fi
+
+	rm -f "${_CLEANUP_FILES[@]}"
+	_CLEANUP_FILES=()
+}
+
+libmktemp_setup() {
+	if [[ $_HAVE_CLEANUP_FILES ]]; then
+		return
+	fi
+
+	declare -g _HAVE_CLEANUP_FILES=1
+	declare -g -a _CLEANUP_FILES
+	ltrap libmktemp_cleanup
+}
+
+libmktemp_use() {
+	libmktemp_setup "$@"
+	mktemp() { libmktemp "$@"; }
+}
