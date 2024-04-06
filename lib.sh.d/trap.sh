intelfx/lib

commit 9e175cac81dcff772f24f2a0c4f8a9ae6b5fcccd

lib.sh: log, trap: add libsh_export_*() to export relevant functions

lib.sh.d/trap.sh
@@ -98,3 +98,20 @@ ltrap_unwind() {
 	done
 	__traps=( "${__traps[@]:$__nr}" )
 }
+
+libsh_export_ltraps() {
+	if [[ ${_LIBSH_HAS_TRAP+set} ]]; then
+		return
+	fi
+	export _LIBSH_HAS_TRAP=1
+	export -f \
+		globaltraps \
+		ltraps \
+		ltrap \
+		luntrap \
+		lruntrap \
+
+	# not exporting mark/unwind because they use logging
+}
+libsh_export_trap() { libsh_export_ltraps; }
+libsh_export_ltrap() { libsh_export_ltraps; }

lib.sh.d/log.sh
@@ -212,3 +212,28 @@ function loud() {
 	done
 	_libsh_log "$prio" "" "" "$header"
 }
+
+libsh_export_log() {
+	if [[ ${_LIBSH_HAS_LOG+set} ]]; then
+		return
+	fi
+	export _LIBSH_HAS_LOG=1
+	export -f \
+		_libsh_printf_var \
+		_libsh_log \
+		_libsh_logf \
+		dbg dbgf \
+		log logf \
+		say sayf \
+		warn warnf \
+		warning warningf \
+		err errf \
+		die dief \
+		xxx xxxf \
+		XXX XXXf \
+		trace \
+		dry_run \
+
+		#check check_e \
+		#assert assert_e \
+}

commit b069e83c60dc80e8c99081ad7bfe6619b203d6ce

lib.sh: trap: export __dynamic_unset()
Fixes: 9e175ca ("lib.sh: log, trap: add libsh_export_*() to export relevant functions")
Fixes: 3170412 ("lib.sh: trap: WTF moment: work around new unset semantics")

lib.sh.d/trap.sh
@@ -122,6 +122,7 @@ libsh_export_ltraps() {
 	fi
 	export _LIBSH_HAS_TRAP=1
 	export -f \
+		__dynamic_unset \
 		globaltraps \
 		ltraps \
 		ltrap \