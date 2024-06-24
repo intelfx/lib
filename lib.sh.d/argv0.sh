709da6344cb7a4c55044ef69e799a9e138f24a1e
Ivan Shapovalov <intelfx@intelfx.name>
2024-06-24 05:38:36 +0200

    lib.sh: argv0: set `$LIB_ARGV0` to probable `$0` adjusting for Linux quirks

---
+++ lib.sh.d/argv0.sh
@@ -0,0 +1,17 @@
+#!/hint/bash
+
+# Remember original $0 for error messages
+if ! [[ ${LIB_ARGV0+set} ]]; then
+	# Apparently, interpreted execution on Linux provides no way
+	# to acquire the original $0 with which the script was invoked.
+	# (https://stackoverflow.com/a/37369285/857932)
+	# Try to re-derive it.
+	for LIB_ARGV0 in "${0##*/}" "$0" "${BASH_SOURCE[2]}"; do
+		if [[ "$(command -v "$LIB_ARGV0")" -ef "${BASH_SOURCE[2]}" ]]; then
+			break
+		fi
+	done
+
+	# NOTE: if $LIB_ARGV0 needs to be inherited across subprocesses,
+	# the caller must export it manually.
+fi
